d6a2a6baf7e20710b6b99114af8989a731681fd8
bismuth01 <siddharthaswarnkar@gmail.com>
2025-09-27 05:08:27 +0530

    fix: Update field name node_data

--- engine/src/schema/NodeSchema.ts
+++ engine/src/schema/NodeSchema.ts
@@ -1,7 +1,7 @@
 export interface NodeSchema {
   label: string;
   type: string;
-  ["node-data"]?: Record<string, any>;
+  node_data?: Record<string, any>;
   inputs: Record<string, any>;
   outputs: Record<string, any>;
 }
