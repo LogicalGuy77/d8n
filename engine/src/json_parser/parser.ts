75cb1e4f4b11cb1d6b5a6b9c263ccc34da7adb18
bismuth01 <siddharthaswarnkar@gmail.com>
2025-09-27 03:20:31 +0530

    feat: Add json workflow parser and test

---
+++ engine/src/json_parser/parser.ts
@@ -0,0 +1,33 @@
+import type { WorkflowSchema } from "../schema/WorkflowSchema.js";
+import { ConditionNode } from "../components/ConditionNode.js";
+import { PythNode } from "../components/PythNode.js";
+import { PrintNode } from "../components/PrintNode.js";
+import { Workflow } from "../components/Workflow.js";
+import type { Edges } from "../interfaces/Edges.js";
+import type { Node } from "../interfaces/Node.js";
+
+export function parse_workflow(json_workflow: WorkflowSchema){
+    const type: string = json_workflow.type;
+    const nodes: Record<string, Node> = {};
+    const edges: Edges = json_workflow.edges;
+    
+    //Parse nodes
+    for(const [id, schema] of Object.entries(json_workflow.nodes)){
+        const nodeData = schema.node_data ?? {};
+        switch(schema.type){
+            case "pyth":
+                nodes[id] = new PythNode(id, schema.label, nodeData.priceFeed);
+                break;
+            case "condition":
+                nodes[id] = new ConditionNode(id, schema.label, schema.inputs, nodeData.condition);
+                break;
+            case "print":
+                nodes[id] = new PrintNode(id, schema.label, schema.inputs);
+                break;
+            default:
+                console.warn(`Unknown node type: ${schema.type} at node ${id}`);
+        }
+    }
+
+    return new Workflow(type, nodes, edges);
+}
--- engine/src/server.ts
+++ engine/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { parse_workflow } from './json_parser/parser.js';
+import { Workflow } from './components/Workflow.js';
 
 const app = express();
 const port = 3000;
@@ -7,6 +9,14 @@ app.use(express.json());
 
 app.get('/status', (req, res) => {
     res.status(200).json({status: "Running"});
+})
+
+app.post('/test', async (req, res) => {
+    const {json_workflow} = req.body;
+    const constructed_workflow: Workflow = parse_workflow(json_workflow);
+
+    await constructed_workflow.start();
+    res.status(200).json({"parsed": "Success"});
 })
 
 app.listen(port, () => {
