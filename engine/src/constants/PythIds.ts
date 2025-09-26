7fe835514100ba65892589aec80da32bbafc5891
bismuth01 <siddharthaswarnkar@gmail.com>
2025-09-27 02:13:18 +0530

    feat: Add constants for Pyth Ids

---
+++ engine/src/constants/PythIds.ts
@@ -0,0 +1,8 @@
+export const PythIds: Record<string, string> = {
+    "BTC_USD" :"0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43",
+    "ETH_USD" : "0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace",
+    "BNB_USD" : "0x2f95862b045670cd22bee3114c39763a4a08beeb663b145d283c31d7d1101c4f",
+    "SOL_USD" : "0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d",
+    "POL_USD" : "0xffd11c5a1cfd42f80afb2df4d9f264c15f956d68153335374ec10722edd70472",
+    "FIL_USD" : "0x150ac9b959aee0051e4091f0ef5216d941f590e1c5e7f91cf7635b5c11628c0e"
+}
