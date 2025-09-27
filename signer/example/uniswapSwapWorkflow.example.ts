aa9eac85c1ba375aa33bbbc71affac9bcbd0dc89
projectman14 <lakshyajain.14022@gmail.com>
2025-09-27 15:48:51 +0000

    feat : Token and Native Transfers added

--- signer/example/uniswapSwapWorkflow.example.ts
+++ signer/example/uniswapSwapWorkflow.example.ts
@@ -10,7 +10,7 @@ import { sepolia } from "viem/chains";
     rpcUrl: process.env.SEPOLIA_RPC_URL as string,
     routerAddress: "0xeE567Fe1712Faf6149d80dA1E6934E354124CfE3", // make sure this is a V2-style router
     tokenIn: "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238",  // USDC (example)
-    tokenOut: "0x7169D38820dfd117C3FA1f22a697dBA58d90BA06", // some token
+    tokenOut: "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9", // some token
     amountIn: 10_000_000n, // 10 USDC (6 decimals)
     amountOutMin: 0n,      // set properly in prod
   });
