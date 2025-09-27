89249859ab3e6d09702fb66c3a7125074d8c265f
bismuth01 <siddharthaswarnkar@gmail.com>
2025-09-27 20:03:30 +0530

    fix: Add walletaddr in WorflowSchema

--- engine/src/schema/WorkflowSchema.ts
+++ engine/src/schema/WorkflowSchema.ts
@@ -2,6 +2,7 @@ import type { NodeSchema } from "./NodeSchema.js";
 import type { EdgesSchema } from "./EdgeSchema.js";
 
 export interface WorkflowSchema {
+    walletaddr: string;
     type: "once" | "repeat";
     nodes: Record<string, NodeSchema>,
     edges: EdgesSchema
